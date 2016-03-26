ef238c5092c880dbdcc6c2ca804d4d5ecb523480
Roman Liutikov <roman01la@romanliutikov.com>
2016-03-26 03:12:23 +0200

    add display name for koans

--- src/koans/core.cljs
+++ src/koans/core.cljs
@@ -1,6 +1,6 @@
 (ns koans.core
   (:use [jayq.core :only [$]]
-        [jayq.util :only [log wait]])
+        [jayq.util :only [wait]])
   (:require
     [cljs.js :as cljs]
     [clojure.set]
@@ -11,11 +11,13 @@
   (:use-macros
     [dommy.macros :only [deftemplate]]))
 
-(defn hash-objects [] (clojure.string/split (.-hash js/location) "/" ))
+(enable-console-print!)
+
+(defn hash-objects [] (clojure.string/split (.-hash js/location) "/"))
 
 (defn current-koan-index [] (meditations/KoanIndex.
-  (subs (first (hash-objects)) 1)
-  (dec (last (hash-objects)))))
+                             (subs (first (hash-objects)) 1)
+                             (dec (last (hash-objects)))))
 
 (defn update-location-hash []
   (let [koan (meditations/next-koan-index (current-koan-index))]
@@ -35,19 +37,19 @@
             [:span {:class "shadow"}]
             [:input {:name "code"}]]
           [:span {:class "text"} text]))]
-      (if-not (nil? (:fn-strings koan))
-        [:div {:class "functions"}
-          (for [function (:fn-strings koan)]
-            [:div {:class "function"}
-              (for [text function]
-                (if (= text :input)
-                  [:span {:class "code"}
-                    [:span {:class "shadow"}]
-                    [:input {:name "code"}]]
-                  [:pre {:class "text"} text]))])])])
+    (if-not (nil? (:fn-strings koan))
+      [:div {:class "functions"}
+        (for [function (:fn-strings koan)]
+          [:div {:class "function"}
+            (for [text function]
+              (if (= text :input)
+                [:span {:class "code"}
+                  [:span {:class "shadow"}]
+                  [:input {:name "code"}]]
+                [:pre {:class "text"} text]))])])])
 
 (deftemplate error-message []
-  [:div {:class "error"} "You have not yet attained enlightenment."])
+  [:div {:class "error"} "Свет впереди, но все еще далеко."])
 
 (defn input-string []
   (letfn [(input-is-empty? [el] (clojure.string/blank? (.-value el)))
@@ -87,11 +89,11 @@
     (when (not (= ($/text $category) current-category))
       ($/fade-out $category))
     (wait fadeout-time (fn []
-      ($/text $category current-category)
-      ($/prepend ($ :body) $elem)
-      ($/fade-in $elem)
-      ($/fade-in $category)
-      (.focus (first ($/find $elem :input)))))))
+                        ($/text $category (:name koan))
+                        ($/prepend ($ :body) $elem)
+                        ($/fade-in $elem)
+                        ($/fade-in $category)
+                        (.focus (first ($/find $elem :input)))))))
 
 (defn render-static-page [selector]
   (remove-active-koan)
@@ -99,18 +101,18 @@
         $other ($ (first (clojure.set/difference #{"#welcome" "#the-end"} #{selector})))]
     ($/fade-out $other)
     (wait fadeout-time (fn []
-      ($/fade-out ($ :.category))
-      ($/fade-in $el)))))
+                        ($/fade-out ($ :.category))
+                        ($/fade-in $el)))))
 
 (defn render-current-koan []
   (cond
     (clojure.string/blank? (.-hash js/location))
-       (render-static-page "#welcome")
+    (render-static-page "#welcome")
     (= (:category (current-koan-index)) "complete")
-      (render-static-page "#the-end")
+    (render-static-page "#the-end")
     (meditations/koan-exists? (current-koan-index))
-      (let [current-koan (meditations/koan-for-index (current-koan-index))]
-        (render-koan current-koan))
+    (let [current-koan (meditations/koan-for-index (current-koan-index))]
+      (render-koan current-koan))
     :else
       (update-location-hash)))
 
@@ -124,9 +126,9 @@
           input-width ($/width $input)]
       (cond
         (>= shadow-width input-width)
-          ($/width $input (+ shadow-width (* 4 char-width)))
+        ($/width $input (+ shadow-width (* 4 char-width)))
         (>= (- input-width (* 4 char-width)) shadow-width)
-          ($/width $input (+ shadow-width (* 4 char-width)))))))
+        ($/width $input (+ shadow-width (* 4 char-width)))))))
 
 (defn show-error-message []
   (let [$code-box ($ :.code-box)]
@@ -135,8 +137,8 @@
         ($/remove-class $code-box "incorrect")
         ($/fade-out $error)
         (wait 300 #(
-          ($/add-class $code-box "incorrect")
-          ($/fade-in $error))))
+                    ($/add-class $code-box "incorrect")
+                    ($/fade-in $error))))
       (let [$error ($ (error-message))]
         ($/add-class $code-box "incorrect")
         ($/after ($ :.code-box) $error)
@@ -147,11 +149,9 @@
 
 (defn evaluate-koan []
   (let [input (input-string)]
-    (log "Evaluating " input)
     (cljs/eval-str compiler-state input nil
       {:eval cljs/js-eval}
       (fn [result]
-        (log result)
         (if (or (:error result) (not= (:value result) true))
           (show-error-message)
           (load-next-koan))))))
@@ -163,13 +163,9 @@
     ($/on $doc :input :input #(resize-input (.-target %))))
   (render-current-koan))
 
-(defn handle-hashchange []
-  (render-current-koan)
-  (js/ga "pageview" (subs (.-hash js/location) 1)))
-
 (defn init []
   ($/document-ready handle-document-ready)
-  (set! (.-onhashchange js/window) handle-hashchange)
+  (set! (.-onhashchange js/window) render-current-koan)
   ;; initialize the cljs.user namespace so that def will actually work
   (cljs/eval compiler-state '(ns cljs.user) {:eval cljs/js-eval} identity))
 
--- src/koans/meditations.cljs
+++ src/koans/meditations.cljs
@@ -20,39 +20,39 @@
     [koans.meditations.datatypes :as datatypes]
     [koans.meditations.partition :as partition]))
 
-(defrecord Koan [description code-strings fn-strings])
+(defrecord Koan [name description code-strings fn-strings])
 (defrecord KoanIndex [category index])
-(defrecord Category [name koans fns])
+(defrecord Category [name link koans fns])
 
 (def categories [
-  (Category. "equality" equality/koans)
-  (Category. "lists" lists/koans)
-  (Category. "vectors" vectors/koans)
-  (Category. "sets" sets/koans)
-  (Category. "maps" maps/koans)
-  (Category. "functions" functions/koans functions/fns)
-  (Category. "conditionals" conditionals/koans conditionals/fns)
-  (Category. "higher-order-functions" higher-order-functions/koans)
-  (Category. "runtime-polymorphism" runtime-polymorphism/koans runtime-polymorphism/fns)
-  (Category. "lazy-sequences" lazy-sequences/koans)
-  (Category. "sequence-comprehensions" sequence-comprehensions/koans)
-  (Category. "creating-functions" creating-functions/koans creating-functions/fns)
-  (Category. "destructuring" destructuring/koans destructuring/fns)
-  (Category. "atoms" atoms/koans atoms/fns)
-  ;(Category. "datatypes" datatypes/koans datatypes/fns)
-  (Category. "partition" partition/koans)
-  (Category. "complete" '(:the :end))
-])
+                 (Category. "равенство" "equality" equality/koans)
+                 (Category. "списки" "lists" lists/koans)
+                 (Category. "векторы" "vectors" vectors/koans)
+                 (Category. "множества" "sets" sets/koans)
+                 (Category. "словари" "maps" maps/koans)
+                 (Category. "функции" "functions" functions/koans functions/fns)
+                 (Category. "условия" "conditionals" conditionals/koans conditionals/fns)
+                 (Category. "функции высшего порядка" "higher-order-functions" higher-order-functions/koans)
+                 (Category. "полиморфизм" "runtime-polymorphism" runtime-polymorphism/koans runtime-polymorphism/fns)
+                 (Category. "ленивые последовательности" "lazy-sequences" lazy-sequences/koans)
+                 (Category. "выражения последовательностей" "sequence-comprehensions" sequence-comprehensions/koans)
+                 (Category. "создание функций" "creating-functions" creating-functions/koans creating-functions/fns)
+                 (Category. "деструктуризация" "destructuring" destructuring/koans destructuring/fns)
+                 (Category. "атомы" "atoms" atoms/koans atoms/fns)
+                ;(Category. "дататипы" "datatypes" datatypes/koans datatypes/fns)
+                 (Category. "разделение" "partition" partition/koans)
+                 (Category. "готово" "complete" '(:the :end))])
+
 
 (defn category-from-koan-index [koan-index]
-  (first (filter #(= (:name %) (:category koan-index)) categories)))
+  (first (filter #(= (:link %) (:category koan-index)) categories)))
 
 (defn koan-exists? [koan-index]
   (< (:index koan-index) (count (partition 2 (:koans (category-from-koan-index koan-index))))))
 
 (defn next-category [koan-index]
   (let [index (inc (utils/index-of (category-from-koan-index koan-index) categories))]
-    (:name (nth categories index))))
+    (:link (nth categories index))))
 
 (defn expr-to-string [expr]
   (if (string? expr)
@@ -63,11 +63,11 @@
   (let [next-in-category (KoanIndex. (:category koan) (inc (:index koan)))]
     (cond
       (koan-exists? next-in-category)
-        next-in-category
+      next-in-category
       (nil? (category-from-koan-index koan))
-        (KoanIndex. "equality" 0)
+      (KoanIndex. "equality" 0)
       (js/isNaN (:index next-in-category))
-        (KoanIndex. (:category koan) 0)
+      (KoanIndex. (:category koan) 0)
       :else
         (KoanIndex. (next-category koan) 0))))
 
@@ -75,18 +75,18 @@
   (let [full-text (expr-to-string expr)
         splitted (clojure.string/split full-text #":__")]
     (apply concat (map (fn [text]
-      (if (= text (last splitted))
-        [text]
-        [text :input]
-      )) splitted))))
+                        (if (= text (last splitted))
+                          [text]
+                          [text :input]))
+                   splitted))))
 
 (defn koan-for-index [koan-index]
   (let [category (category-from-koan-index koan-index)
         category-list (partition 2 (:koans category))
         item (try
-          (nth category-list (:index koan-index))
-          (catch js/Object _ (first category-list)))
+              (nth category-list (:index koan-index))
+              (catch js/Object _ (first category-list)))
         description (first item)
         code-strings (expr-to-array (last item))
         fn-strings (map #(expr-to-array %) (:fns category))]
-    (Koan. description code-strings fn-strings)))
+    (Koan. (:name category) description code-strings fn-strings)))
